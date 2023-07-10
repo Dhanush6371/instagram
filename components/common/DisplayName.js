1860dbd30f65638f7c2bd0c99c9edb50edb42ffb
Dhanush6371 <98277180+Dhanush6371@users.noreply.github.com>
2023-07-10 12:12:28 +0530

    Add files via upload

---
+++ components/common/DisplayName.js
@@ -0,0 +1,15 @@
+import Router from 'next/router'
+
+
+const DisplayName = ({ username, user }) => {
+
+    return (
+        <p
+            className='text-14-bold mr-1 cursor-pointer'
+        >
+            {username}
+        </p>
+    )
+}
+
+export default DisplayName
---
+++ static/data.js
@@ -0,0 +1,45 @@
+export const data = {
+    stories: [
+        
+        {
+            username: 'zuck',
+            image:
+                'https://i.insider.com/6329911dedbe7f00190ef740?width=1136&format=jpeg',
+        },
+        {
+            username: 'elonmusk',
+            image:
+                'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg',
+        },
+        {
+            username: 'mkbhd',
+            image: 'https://pbs.twimg.com/media/EK2imELWwAA3FQt.jpg:large',
+        },
+
+        {
+            username: 'mrbeast',
+            image:
+                'https://yt3.googleusercontent.com/ytc/AMLnZu9UlPHTbLrgfwt0ucUKik9Z8DfO6dVgVnfum2o9hA=s176-c-k-c0x00ffffff-no-rj-mo',
+        },
+    ],
+    suggestions: [
+        
+        {
+            username: 'zuck',
+            image:
+                'https://i.insider.com/6329911dedbe7f00190ef740?width=1136&format=jpeg',
+            text: 'Followed by cleverqazi + 2 more',
+        },
+        {
+            username: 'elonmusk',
+            image:
+                'https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg',
+            text: 'Followed by cleverqazi + 10 more',
+        },
+        {
+            username: 'mkbhd',
+            image: 'https://pbs.twimg.com/media/EK2imELWwAA3FQt.jpg:large',
+            text: 'Followed by elonmusk + 7 more',
+        },
+    ],
+}
